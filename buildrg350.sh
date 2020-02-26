9300dcfe71fc7cf02ac8421a7a895d0c5a53660d
hiroshica <hiroshica@gmail.com>
2020-02-26 22:13:15 +0900

    adding RG350 keymap

--- buildrg350.sh
+++ buildrg350.sh
@@ -11,4 +11,4 @@ export PKG_CONFIG_PATH=$TCBASE/bin
 export CROSS_COMPILE=mipsel-linux-
 export CFLAGS="-I$TC/usr/include -I$TC/usr/include/SDL"
 export LDFLAGS="--sysroot $TC -L$TC/lib"
-./configure --platform=opendingux && make clean && make opk
+./configure --platform=rg350 && make clean && make opk
