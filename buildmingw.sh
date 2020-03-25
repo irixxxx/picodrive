06c8d28ed8a6ec1de2428628a77b3a1b21d02ce1
hiroshica <hiroshica@gmail.com>
2020-03-25 17:44:33 +0900

    delete commit mesg.

---
+++ buildmingw.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+export TCBASE=/mingw64
+export TC=$TCBASE
+export CROSS_LIB_PATH=$TCBASE/lib
+#export PATH=$TCBASE/bin:$PATH
+export PKG_CONFIG_PATH=$TCBASE/bin
+#
+#
+#
+#export CROSS_COMPILE=mipsel-linux-
+export CFLAGS="-I$TC/include"
+export LDFLAGS="--sysroot $TC -L$TC/lib"
+export PKG_CONFIG=$TCBASE/bin/pkg-config.exe
+./configure --platform=mingw && make clean && make opk
