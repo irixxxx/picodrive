20a3ca1a385d0b1e57a43a5418a17c0a41c6ca59
kub <derkub@gmail.com>
2026-08-17 22:44:16 +0200

    32x, another sync fix

--- pico/32x/32x.c
+++ pico/32x/32x.c
@@ -530,7 +530,7 @@ void p32x_sync_other_sh2(SH2 *sh2, unsigned int m68k_target)
 }
 
 #define STEP_LS 24
-#define STEP_N 192 // NFL; TODO at least a scanline (489) for good performance?
+#define STEP_N 164 // NFL, Xmen; TODO at least a scanline (489) for good perf?
 
 #define sync_sh2s_normal p32x_sync_sh2s
 //#define sync_sh2s_lockstep p32x_sync_sh2s
